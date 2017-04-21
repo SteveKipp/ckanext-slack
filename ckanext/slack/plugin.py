8904e449c57627a369c38610da5503ecb136d8d0
Steve Kipp <Steven.Kipp@ogsystems.com>
2017-04-20 19:02:37 -0500

    posts to slack on update to dataset

--- ckanext/slack/plugin.py
+++ ckanext/slack/plugin.py
@@ -9,6 +9,11 @@ BOT_NAME = 'ckan_bot'
 print(os.environ.get('SLACK_BOT_TOKEN'))
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+# starterbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+
 class SlackPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IMapper)
@@ -19,20 +24,24 @@ class SlackPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'slack')
 
+    def talk(self, channel):
+        """
+            Receives commands directed at the bot and determines if they
+            are valid commands. If so, then acts on the commands. If not,
+            returns back what it needs for clarification.
+        """
+        msg = "Hi, good news! and update to a dataset has been made!"
+        slack_client.api_call("chat.postMessage", channel=channel,
+                              text=msg, as_user=True)
+
     def before_update(self, mapper, connection, instance):
         pass
 
     def after_update(self, mapper, connection, instance):
         #pdb.set_trace()
         print("this has run")
-        api_call = slack_client.api_call('users.list')
-        if api_call.get('ok'):
-            users = api_call.get('members')
-            for user in users:
-                if 'name' in user and user.get('name') == BOT_NAME:
-                    print('bot_id found {}:{}'.format(user['name'], user.get('id')))
-        else:
-            print('could not find that bot')
+        self.talk('general')
+
 
 
     def before_insert(self, mapper, connection, instance):
